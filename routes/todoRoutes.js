9e9df59110cdb5a5791a95b09a612c4f75b9a500
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-05-04 00:06:57 -0300

    feat: add /put/posts/:id

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -17,25 +17,26 @@ const Todo = require("../models/todo");
  * @returns {string}
  */
 const createTodoTemplate = (todo) => `
-<li id="todo-${
-    todo.id
-}" class="list-group-item d-flex justify-content-between align-items-center">
-    <span>${todo.title}</span>
-    <div>
-        <form class="d-inline">
-            <button class="btn btn-link">
-                ${todo.completed ? "Desmarcar" : "Marcar conclusão"}
-            </button>
-        </form/>
-        <form class="d-inline" hx-delete="/api/todos/${
-            todo.id
-        }"  hx-target="#todo-list">
-            <button class="btn btn-danger"  >
-                Excluir
-            </button>
-        </form/>
-    </div>
-</li>
+    <li id="todo-${
+        todo.id
+    }" class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo.title}</span>
+        <div>
+            <form class="d-inline" hx-put="/api/todos/${
+                todo.id
+            }" hx-target="#todo-${todo.id}" hx-swap="outerHTML">
+                <input type="hidden" name="completed" value="${!todo.completed}">
+                <button type="submit" class="btn btn-link p-0">
+                    ${todo.completed ? "Desmarcar" : "Marcar conclusão"}
+                </button>
+            </form>
+            <form class="d-inline" hx-delete="/api/todos/${
+                todo.id
+            }" hx-target="#todo-${todo.id}" hx-swap="outerHTML">
+                <button class="btn btn-danger btn-sm">Excluir</button>
+            </form>
+        </div>
+    </li>
 `;
 
 router.get("/todos", async (req, res) => {
@@ -66,6 +67,21 @@ router.delete("/todos/:id", async (req, res) => {
     const todoItems = todos.map(createTodoTemplate).join("");
 
     res.send(todoItems);
+});
+
+router.put("/todos/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const { completed } = req.body;
+
+    await Todo.update(
+        {
+            completed: JSON.parse(completed),
+        },
+        { where: { id } }
+    );
+    const updatedTodo = await Todo.findByPk(id);
+    res.send(createTodoTemplate(updatedTodo));
 });
 
 module.exports = router;
