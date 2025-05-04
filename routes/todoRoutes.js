ab37bf0322e15903d492678787668e2b537d22f1
jairo-sousa <jairo.sousa.rufino@gmail.com>
2025-05-03 23:32:19 -0300

    feat: load todo items

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,6 +4,46 @@ const router = express.Router();
 const { sequelize } = require("sequelize");
 const Todo = require("../models/todo");
 
+/**
+ * @typedef {Object} TodoModel
+ * @property {number|string} id - unique identifier
+ * @property {string} title - Title/description of todo
+ * @property {boolean} completed - Indicates finished todo
+ */
+
+/**
+ * Create html template for todo object
+ * @param {TodoModel} todo
+ * @returns {string}
+ */
+const createTodoTemplate = (todo) => `
+<li id="todo-${
+    todo.id
+}" class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo.title}</span>
+    <div>
+        <form class="d-inline">
+            <button class="btn btn-link">
+                ${todo.completed ? "Desmarcar" : "Marcar conclusão"}
+            </button>
+        </form/>
+        <form class="d-inline">
+            <button class="btn btn-danger" >
+                Excluir
+            </button>
+        </form/>
+    </div>
+</li>
+`;
+
+router.get("/todos", async (req, res) => {
+    const todos = await Todo.findAll();
+
+    const todoItems = todos.map(createTodoTemplate).join("");
+
+    res.send(todoItems);
+});
+
 router.post("/todos", async (req, res) => {
     const { title } = req.body;
 
@@ -11,7 +51,7 @@ router.post("/todos", async (req, res) => {
         title,
     });
 
-    res.send("Todo criado!");
+    res.send(createTodoTemplate(newTodo));
 });
 
 module.exports = router;
